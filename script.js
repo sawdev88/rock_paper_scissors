12640b59c3c8deea064e4e05814fd6f704c3552e
Sean Wines <seanwinesdev@gmail.com>
2016-08-02 20:07:28 -0700

    Fun little game

--- script.js
+++ script.js
@@ -2,40 +2,78 @@ var compPickArr = ['Rock', 'Paper', 'Scissors'],
     userPick,
     compPick,
     winnerStr,
-    randomNumber = Math.floor(Math.random() * compPickArr.length);
+    userScore = 0,
+    compScore = 0,
+    areYouAWinner,
+    $userScoreText = $('.user-score-text'),
+    $compScoreText = $('.comp-score-text'),
+    $winnerText = $('.winner-text'),
+    $compText = $('.comp-text'),
+    $playAgainBtn = $('.play-again');
 
-function compChoice() {
-  compPick = compPickArr[randomNumber];
+function compChoice(item) {
+    compPick = compPickArr[Math.floor(Math.random() * compPickArr.length)];
 }
 
-function winner(userPick, compPick){
+function winner(userPick, compPick) {
 
-  if (userPick === compPick) {
-    winnerStr = 'Its a tie';
-  } else if (userPick === 'Rock' && compPick === 'Scissors') {
-    winnerStr = 'You Win';
-  } else if (userPick === 'Paper' && compPick === 'Rock') {
-    winnerStr = 'You Win';
-  } else if (userPick === 'Scissors' && compPick === 'Paper') {
-    winnerStr = 'You Win';
-  } else {
-    winnerStr = "You lose";
-  }
+    if (userPick === compPick) {
+        winnerStr = 'Tie';
+        areYouAWinner = null;
+    } else if (userPick === 'Rock' && compPick === 'Scissors') {
+        winnerStr = 'Win';
+        areYouAWinner = true;
+    } else if (userPick === 'Paper' && compPick === 'Rock') {
+        winnerStr = 'Win';
+        areYouAWinner = true;
+    } else if (userPick === 'Scissors' && compPick === 'Paper') {
+        winnerStr = 'Win';
+        areYouAWinner = true;
+    } else {
+        winnerStr = "loser";
+        areYouAWinner = false;
+    }
 
 }
 
-$(function () {
+function makePick() {
+    $('.play').on('click', function() {
+        userPick = $(this).val();
+        compChoice();
 
-  $('button').on('click', function () {
-    userPick = $(this).val();
-    compChoice();
-    console.log('USER: ' + userPick + ' COMP: ' + compPick);
+        winner(userPick, compPick);
+        $winnerText.text(winnerStr)
+        $compText.text('Computer picked: ' + compPick);
 
-    winner(userPick, compPick);
+        if ($compText.text().length > 0) {
+            $('.play').attr('disabled', 'disabled');
+            $playAgainBtn.fadeIn();
+            compChoice();
+        }
+    })
+}
 
-    console.log(winnerStr);
+$(function() {
 
-  })
+    makePick();
 
+    $playAgainBtn.on('click', function() {
+        $('.play').removeAttr('disabled');
+        $compText.text('');
+        $(this).hide();
+
+        if (areYouAWinner) {
+            ++userScore;
+            $winnerText.text('Nice Win. Keep it going.');
+            $userScoreText.text(userScore);
+        } else if (areYouAWinner === false) {
+            ++compScore;
+            $winnerText.text('Tough loss. Get a win this time.');
+            $compScoreText.text(compScore);
+        } else {
+            $winnerText.text('Ties are okay. Get \'em this time');
+        }
+        makePick();
+    })
 
 })
